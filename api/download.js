de7cf227410d85df19683a38d565182784e2df5d
masknetdesign <masknetdesign@gmail.com>
2024-06-13 22:28:56 -0300

    Initial commit

--- api/download.js
+++ api/download.js
@@ -19,15 +19,13 @@ app.get('/api/download', async (req, res) => {
         res.header('Content-Disposition', `attachment; filename="${videoTitle}.mp4"`);
         ytdl(url, { format: 'mp4' }).pipe(res);
     } catch (error) {
-        console.error('Erro ao processar o download do vídeo:', error);
+        console.error('Erro ao obter informações do vídeo:', error);
         res.status(500).send('Erro ao processar o download do vídeo');
     }
 });
 
 // Rota para servir o arquivo HTML
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-});
+app.use(express.static(path.join(__dirname, '../public')));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
